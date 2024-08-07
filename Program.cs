32f416cf950033e01f112a60102d84a5d4f4a33e
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-08-07 12:17:31 -0700

    update

--- Program.cs
+++ Program.cs
@@ -8,51 +8,90 @@ namespace Array_Console_App
         static void Main(string[] args)
         {
             ////begin program
-            //Console.WriteLine("Welcome to the Array App");
-            //Console.ReadLine();
+            Console.WriteLine("Welcome to the Array App");
+            Console.ReadLine();
 
             ////array of strings and user input
             Console.WriteLine("Select an index from 0 to 3 and a corresponding car will appear.");
-            Console.ReadLine();
-            string[] MyCars = { "Chevy", "Ford", "Toyota", "Dodge" };
-            int stringIndex1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(MyCars[0]);
-            Console.WriteLine(MyCars[1]);
-            Console.WriteLine(MyCars[2]);
-            Console.WriteLine(MyCars[3]);
-            Console.ReadLine();
+            List<string> MyCar = new List<string>();
+            MyCar.Add("Tesla");
+            MyCar.Add("Rivian");
+            MyCar.Add("Lucid");
+            MyCar.Add("Ford");
 
-
-            //////array of integers and user input
-            Console.WriteLine("Select an index from 0 to 3 and a corresponding number in an array will appear.");
-            Console.ReadLine();
-            int[] MyIntegers = { 77, 140596, 3340, 2349304 };
-            
-            Console.WriteLine(MyIntegers[0]);
-            Console.WriteLine(MyIntegers[1]);
-            Console.WriteLine(MyIntegers[2]);
-            Console.WriteLine(MyIntegers[3]);
-            Console.ReadLine();
+            int index = int.Parse(Console.ReadLine());
             
 
-            ////List of strings and user input
+            //if else statement 
+            if (index == 0 || index == 1 || index == 2 || index == 3)
+
+            {
+                Console.WriteLine("You chose: " + MyCar[index]);
+            }
+
+            else
+
+            {
+                Console.WriteLine("Error. Only enter numbers 0 - 3");
+            }
+            
+            Console.ReadLine();
+
+
+
+
+            //array of integers and user input
+            double[] MyIntegers = new double[] { 77, 140598, 3345, 2341654, 89564 };
+            Console.WriteLine("Please select the index of the array");
+
+            int index1 = int.Parse(Console.ReadLine());
+
+            //if else statement
+            if (index1 == 0 || index1 == 1 || index1 == 2 || index1 == 3 || index1 == 4) 
+
+            {
+                Console.WriteLine("You chose: " + MyIntegers[index1]);
+            }
+
+            else 
+            
+            {
+                Console.WriteLine("Error. Only enter numbers 0 - 4");
+            }
+
+            Console.ReadLine();
+
+
+
+
+            //List of strings and user input
             Console.WriteLine("Select an index of 0 to 3 and a phrase will appear.");
-            Console.ReadLine();
+            
             List<string> Phrase = new List<string>();
             Phrase.Add("Hahaha. What a story, Mark!");
             Phrase.Add("Oh hi, Mark.");
             Phrase.Add("You are tearing me apart, Lisa!");
             Phrase.Add("Leave your stupid comments in your pocket!");
 
-            Console.WriteLine(Phrase[0]);
-            Console.WriteLine(Phrase[1]);
-            Console.WriteLine(Phrase[2]);
-            Console.WriteLine(Phrase[3]);
+            int index2 = int.Parse(Console.ReadLine());
+            
 
-            int stringIndex2 = Convert.ToInt32(Console.ReadLine());
+            //if else statement
+            if (index2 == 0 || index2 == 1 || index2 == 2 || index2 == 3) 
 
+            {
+                Console.WriteLine("You chose: " + Phrase[index2]);
+            }
+
+            else 
+
+            {
+               Console.WriteLine("Error. Only enter numbers 0 - 3");
+            }
             Console.ReadLine();
+
+            
 
 
         }
